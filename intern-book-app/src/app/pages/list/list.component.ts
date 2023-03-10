43f99d2133a3316c24c1a6c18a1a31384437a344
ak03120 <ak03120@aol.com>
2023-03-10 12:04:40 +0900

    一覧画面に書籍一覧を表示しました

--- intern-book-app/src/app/pages/list/list.component.ts
+++ intern-book-app/src/app/pages/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Book } from 'src/app/types/book';
 
 @Component({
   selector: 'app-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
-
+  bookList: Book[] = [
+    { name: 'アンドロイドは電気羊の夢を見るか?', detail: '第三次大戦後の未来、サンフランシスコを舞台に賞金稼ぎのリック・デッカードが火星から逃亡してきた8体のアンドロイドを「処理」するというあらすじ', evaluation: 90 },
+    { name: '岩田さん: 岩田聡はこんなことを話していた。', detail: '任天堂の元社長、岩田聡さんのことばをまとめた本', evaluation: 90 }
+  ];
 }
