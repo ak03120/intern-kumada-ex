a5d3e53eb09bfe84ca7e863f16f090e26efebf63
ak03120 <ak03120@aol.com>
2023-03-23 14:09:00 +0900

    ダイアログ用のコンポーネントを追加

--- intern-book-app/src/app/app.module.ts
+++ intern-book-app/src/app/app.module.ts
@@ -1,17 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatCardModule} from '@angular/material/card';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { ListComponent } from './pages/list/list.component';
-import { CardComponent } from './components/card/card.component';
+import { CardComponent, DialogComponent } from './components/card/card.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
@@ -19,7 +20,8 @@ import { FormsModule } from '@angular/forms';
     AppComponent,
     ToolbarComponent,
     ListComponent,
-    CardComponent
+    CardComponent,
+    DialogComponent
   ],
   imports: [
     MatToolbarModule,
@@ -29,7 +31,8 @@ import { FormsModule } from '@angular/forms';
     MatCardModule,
     MatInputModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
