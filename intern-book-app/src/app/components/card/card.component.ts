4b70f044b020b7cfc933c7770da26d5bd8bc2503
ak03120 <ak03120@aol.com>
2023-03-23 14:09:21 +0900

    削除する前にダイアログを表示

--- intern-book-app/src/app/components/card/card.component.ts
+++ intern-book-app/src/app/components/card/card.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, Inject } from '@angular/core';
 import { Book } from 'src/app/types/book';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-card',
@@ -10,8 +12,43 @@ import { Book } from 'src/app/types/book';
 export class CardComponent {
   @Input() book?: Book;
   @Output() deleteCardEvent = new EventEmitter<number>();
+  bookId: number = 0;
 
+  constructor(private dialog:MatDialog){
+  }
+  
   deleteCard(id: number) {
     this.deleteCardEvent.emit(id);
   }
+  
+  deleteDialog(targetBook: Book) {
+    this.dialog
+    .open(DialogComponent, {data: targetBook})
+    .afterClosed()
+    .subscribe(result => {
+      if(result == "1") this.deleteCard(targetBook.id);
+    });
+  }
+}
+
+@Component({
+  selector: 'app-dialog',
+  template: `
+  <h2 mat-dialog-title>本を削除しますか？</h2>
+  <mat-dialog-content>
+    <p>{{data.name}}</p>
+  </mat-dialog-content>
+  <mat-dialog-actions align="end">
+    <button mat-button mat-dialog-close="0">キャンセル</button>
+    <button mat-button color="warn" mat-dialog-close="1">削除</button>
+  </mat-dialog-actions>
+`
+})
+
+export class DialogComponent {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
 }
