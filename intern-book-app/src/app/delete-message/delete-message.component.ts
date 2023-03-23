1f1cc8dba3df18c37b976e3183abe6465552dda1
ak03120 <ak03120@aol.com>
2023-03-23 19:19:50 +0900

    delete-message.component.htmlにテンプレートを移動

--- intern-book-app/src/app/delete-message/delete-message.component.ts
+++ intern-book-app/src/app/delete-message/delete-message.component.ts
@@ -3,16 +3,7 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-delete-message',
-  template: `
-  <h2 mat-dialog-title>本を削除しますか？</h2>
-  <mat-dialog-content>
-    <p>{{data.name}}</p>
-  </mat-dialog-content>
-  <mat-dialog-actions align="end">
-    <button mat-button mat-dialog-close="0">キャンセル</button>
-    <button mat-button color="warn" mat-dialog-close="1">削除</button>
-  </mat-dialog-actions>
-`,
+  templateUrl: './delete-message.component.html',
   styleUrls: ['./delete-message.component.scss']
 })
 
