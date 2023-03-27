efa35bca63a95cc492694a6bdd72e7c8f63a0896
ak03120 <ak03120@aol.com>
2023-03-27 11:04:25 +0900

    操作ログが表示されるようにしました

--- intern-book-app/src/app/components/card/card.component.ts
+++ intern-book-app/src/app/components/card/card.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, Output, EventEmitter, Inject } from '@angular/core';
 import { Book } from 'src/app/types/book';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { DeleteMessageComponent } from 'src/app/delete-message/delete-message.component';
+import { MessageService } from '../message/message.service';
 
 
 @Component({
@@ -11,11 +12,12 @@ import { DeleteMessageComponent } from 'src/app/delete-message/delete-message.co
 })
 
 export class CardComponent {
+  
   @Input() book?: Book;
   @Output() deleteCardEvent = new EventEmitter<number>();
   bookId: number = 0;
 
-  constructor(private dialog:MatDialog){
+  constructor(private dialog: MatDialog, private messageService: MessageService){
   }
 
   deleteCard(id: number) {
@@ -28,6 +30,7 @@ export class CardComponent {
     .afterClosed()
     .subscribe(result => {
       if(result == "1") this.deleteCard(targetBook.id);
+      this.messageService.add(`${CardComponent.name}: 本「${targetBook.name}」を削除しました。`);
     });
   }
 }
