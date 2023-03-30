344b73d2612679b5fb99c095b145215fa6fea5f3
ak03120 <ak03120@aol.com>
2023-03-30 18:16:18 +0900

    コンフリクトの解消をし、マージしました

--- intern-book-app/src/app/components/card/card.component.ts
+++ intern-book-app/src/app/components/card/card.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, Output, EventEmitter, Inject } from '@angular/core';
 import { Book } from 'src/app/types/book';
-import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { DeleteMessageComponent } from 'src/app/delete-message/delete-message.component';
+import { MessageService } from '../message/message.service';
 
 
 @Component({
@@ -11,10 +12,11 @@ import { DeleteMessageComponent } from 'src/app/delete-message/delete-message.co
 })
 
 export class CardComponent {
+
   @Input() book?: Book;
   @Output() deleteCardEvent = new EventEmitter<number>();
 
-  constructor(private dialog:MatDialog){
+  constructor(private dialog: MatDialog, private messageService: MessageService){
   }
 
   deleteCard(id: number) {
@@ -27,6 +29,7 @@ export class CardComponent {
     .afterClosed()
     .subscribe(result => {
       if(result === "1") this.deleteCard(targetBook.id);
+      this.messageService.add(`${CardComponent.name}: 本「${targetBook.name}」を削除しました。`);
     });
   }
 }
